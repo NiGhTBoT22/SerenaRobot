1dbe31663d756edd41839e6f2ada78bb1af3bef6
NiGhT_BoT_22 <88623043+NiGhTBoT22@users.noreply.github.com>
2021-12-28 15:40:34 +0530

    Update config.py

--- SerenaRobot/config.py
+++ SerenaRobot/config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/AuTo_BoTs_RoBoT/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/SerenaRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
